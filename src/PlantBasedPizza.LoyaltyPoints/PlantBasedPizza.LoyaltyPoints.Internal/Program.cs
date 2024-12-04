02c807a1977f78b1810f766ccd99058e13db2793
huydq2152 <quanghuy2152@gmail.com>
2024-12-04 23:14:44 +0700

    implement simple payment service with GRPC

--- src/PlantBasedPizza.LoyaltyPoints/PlantBasedPizza.LoyaltyPoints.Internal/Program.cs
+++ src/PlantBasedPizza.LoyaltyPoints/PlantBasedPizza.LoyaltyPoints.Internal/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using PlantBasedPizza.LoyaltyPoints.Internal.Services;
 using PlantBasedPizza.LoyaltyPoints.Shared;
 
